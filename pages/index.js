c7c276e6e1d4f2744eb3342c79dadae65438e9b3
AntonioJesusRG <antoniogar129@gmail.com>
2023-05-23 17:02:03 +0200

    Change home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 import Layout from '@/components/Layout/Layout';
+import {useSession} from 'next-auth/react'
+import Image from 'next/image';
 
 export default function Home() {
-  return <Layout>test</Layout>
+
+  const {data: session} = useSession();
+  return (
+    <Layout className='text-blue-900 flex'>
+      <div className='text-blue-900 flex justify-between'>
+        <h2>Hello, {session?.user?.name}</h2>
+        <div className='flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden'>
+          <Image
+          className='w-6 h-6 '
+            src={session?.user?.image}
+            alt='User image'
+            width={50}
+            height={50}
+          ></Image>
+          <span className='px-2'>{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
 }
