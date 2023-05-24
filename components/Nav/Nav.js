44315ddfb7398982f7c01e81bee47803514fd71b
AntonioJesusRG <antoniogar129@gmail.com>
2023-05-24 23:53:32 +0200

    Add categories link

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -43,7 +43,9 @@ export default function Nav() {
 				</Link>
 				<Link
 					href={'/products'}
-					className={router.pathname.includes('/products') ? activeLink : inactiveLink}
+					className={
+						router.pathname.includes('/products') ? activeLink : inactiveLink
+					}
 				>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
@@ -58,6 +60,33 @@ export default function Nav() {
 						/>
 					</svg>
           Products
+				</Link>
+				<Link
+					href={'/categories'}
+					className={
+						router.pathname.includes('/categories') ? activeLink : inactiveLink
+					}
+				>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+						strokeWidth={1.5}
+						stroke='currentColor'
+						className='w-6 h-6'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z'
+						/>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M6 6h.008v.008H6V6z'
+						/>
+					</svg>
+          Categories
 				</Link>
 				<Link
 					href={'/orders'}
@@ -79,7 +108,9 @@ export default function Nav() {
 				</Link>
 				<Link
 					href={'/settings'}
-					className={router.pathname === '/settings' ? activeLink : inactiveLink}
+					className={
+						router.pathname === '/settings' ? activeLink : inactiveLink
+					}
 				>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
