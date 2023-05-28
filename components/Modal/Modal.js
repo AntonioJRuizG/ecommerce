da1c78e2a02f8e0e5e0d3f4bca5ff8aff4382f1e
AntonioJesusRG <antoniogar129@gmail.com>
2023-05-29 01:53:11 +0200

    Add basic modal component

---
+++ components/Modal/Modal.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+
+export default function Modal({category, setDeletedCategory, deletedCategory, deleteCategory}) {
+	const [showModal, setShowModal] = useState(false);
+
+	const handleClickLeftBtn = () => {
+		setDeletedCategory(category);
+		setShowModal(true);
+	};
+
+	const handleClick = () => {
+		deleteCategory(deletedCategory);
+		setShowModal(false);
+	};
+
+	return (
+		<>
+			<button
+				className='btn-primary'
+				type='button'
+				onClick={handleClickLeftBtn}
+			>
+        Delete
+			</button>
+			{showModal ? (
+				<div className='flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-blur-sm'>
+					<div className='relative w-auto my-6 mx-auto max-w-3xl border-0 rounded-md shadow-lg flex flex-col bg-white outline-none focus:outline-none'>
+						<p className='text-center text-lg font-semibold p-6'>
+              Do you really want to delete?
+						</p>
+
+						<div className='flex items-center justify-center p-6 gap-2'>
+							<button
+								className='btn-primary'
+								type='button'
+								onClick={() => setShowModal(false)}
+							>
+                No
+							</button>
+							<button className='btn-red' type='button' onClick={handleClick}>
+                Yes, delete
+							</button>
+						</div>
+					</div>
+				</div>
+			) : null}
+		</>
+	);
+}
