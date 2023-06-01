59fb011f21782f7e406e6f70315b3137543f1cf5
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-01 16:17:07 +0200

    Add properties to api methods

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -3,24 +3,24 @@ import {Category} from '@/models/category';
 export default async function handle(req, res) {
 	const {method} = req;
 
-	if (method === 'POST') {
-		const {name, parentCategory} = req.body;
-		const parent = parentCategory ? parentCategory : null;
-		const categoryDoc = await Category.create({name, parent});
-		res.json(categoryDoc);
-	}
-
 	if (method === 'GET') {
 		res.json(await Category.find().populate('parent'));
 	}
 
+	if (method === 'POST') {
+		const {name, parentCategory, properties} = req.body;
+		const categoryDoc = await Category.create({name, parent: parentCategory || undefined, properties});
+		res.json(categoryDoc);
+	}
+
 	if (method === 'PUT') {
-		const {id, name, parentCategory} = req.body;
+		const {id, name, parentCategory, properties} = req.body;
 		const categoryDoc = await Category.findByIdAndUpdate(
 			{_id: id},
 			{
+				parent: parentCategory || undefined,
+				properties,
 				name,
-				parent: parentCategory,
 			},
 		);
 		res.json(categoryDoc);
