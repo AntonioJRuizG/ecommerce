04c8c901e36c891fdcc067e132ad8aa6e9dfb3ef
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-04 01:26:58 +0200

    Change title and meta description

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import '../styles/globals.css';
 import {SessionProvider} from 'next-auth/react';
+import Head from 'next/head';
 import React from 'react';
 
 export default function App({Component, pageProps: {session, ...pageProps}}) {
 	return (
-		<SessionProvider session={session}>
-			<Component {...pageProps} />
-		</SessionProvider>);
+		<>
+			<Head>
+				<title>Ecommerce</title>
+				<meta
+					name='description'
+					content='Electronics products at the best price. Ecommerce'
+				/>
+			</Head>
+			<SessionProvider session={session}>
+				<Component {...pageProps} />
+			</SessionProvider>
+		</>
+	);
 }
