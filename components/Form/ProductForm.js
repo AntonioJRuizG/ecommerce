586a74da9981916cd32ef68ae830805ba226e741
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-04 03:35:53 +0200

    Add error managment in products editing

--- components/Form/ProductForm.js
+++ components/Form/ProductForm.js
@@ -9,6 +9,9 @@ import SortableList, {SortableItem} from 'react-easy-sort';
 import arrayMove from 'array-move';
 
 export default function ProductForm({id, ...props}) {
+	const [showModal, setShowModal] = useState(false);
+	const [fetchSuccess, setFetchSuccess] = useState(false);
+
 	const router = useRouter();
 	const [title, setTitle] = useState(props.title || '');
 	const [description, setDescription] = useState(props.description || '');
@@ -45,13 +48,19 @@ export default function ProductForm({id, ...props}) {
 		}
 
 		if (id) {
-			await axios.put('/api/products', {...data, id});
+			await axios
+				.put('/api/products', {...data, id})
+				.then(setFetchSuccess(true))
+				.catch(_error => setFetchSuccess(false));
 		} else {
-			await axios.post('/api/products', data);
+			await axios
+				.post('/api/products', data)
+				.then(setFetchSuccess(true))
+				.catch(_error => setFetchSuccess(false));
 		}
 
 		setIsUploagin(false);
-		setGoBack(true);
+		setShowModal(true);
 	};
 
 	if (goBack) {
@@ -96,136 +105,179 @@ export default function ProductForm({id, ...props}) {
 	};
 
 	return (
-		<form onSubmit={createProduct}>
-			<label>Product name</label>
-			<input
-				type='text'
-				placeholder='product name'
-				value={title}
-				onChange={ev => setTitle(ev.target.value)}
-			></input>
-			<label>Category</label>
-			<select value={category} onChange={ev => setCategory(ev.target.value)}>
-				<option value=''>Uncategorized</option>
-				{categoriesList.length > 0
-					? categoriesList.map(categoryItem => (
-						<option key={categoryItem._id} value={categoryItem._id}>
-							{categoryItem.name}
-						</option>
-					))
-					: null}
-			</select>
-			<div className='pt-2'>
-				{propertiesToFill.length > 0
-					? propertiesToFill.map(p => (
-						<div key={p.name} className='flex flex-row items-baseline gap-1'>
-							<div className='flex'>
-								<label>{p.name[0].toUpperCase() + p.name.substring(1)}</label>
-							</div>
-							<div className='flex'>
-								<select
-									value={productProperties[p.name]}
-									onChange={ev => setProductProps(p.name, ev.target.value)}
-								>
-									<option value=''>Property value</option>
-									{p.values.map(value => (
-										<option key={value} value={value}>
-											{value}
-										</option>
-									))}
-								</select>
-							</div>
-						</div>
-					))
-					: null}
-			</div>
-
-			<label>Images</label>
-			<div className='flex gap-1 flex-row flex-wrap mb-3'>
-				<label className='text-primary w-24 h-24 border flex items-center justify-center text-sm gap-1 rounded-sm bg-white border-primary shadow-sm cursor-pointer'>
-          Upload{' '}
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						fill='none'
-						viewBox='0 0 24 24'
-						strokeWidth='1.5'
-						stroke='currentColor'
-						className='w-4 h-4'
-					>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5'
-						/>
-					</svg>
-					<input type='file' className='hidden'></input>
-				</label>
-				{isUploading ? (
-					<label className='w-24 h-24 border flex items-center justify-center text-sm gap-1 rounded-sm bg-gray-200'>
-						<div className='activityIndicator'></div>
-					</label>
-				) : null}
-
-				<SortableList
-					onSortEnd={onSortEnd}
-					className='list flex gap-1 flex-row flex-wrap'
-					draggedItemClassName='dragged'
+		<>
+			<form onSubmit={createProduct}>
+				<label>Product name</label>
+				<input
+					type='text'
+					placeholder='product name'
+					value={title}
+					onChange={ev => setTitle(ev.target.value)}
+				></input>
+				<label>Category</label>
+				<select
+					value={category}
+					onChange={ev => setCategory(ev.target.value)}
 				>
-					{images?.length > 0
-						? images.map(image => (
-							<div key={image}>
-								<div className='p-0 m-0 block relative'>
-									<button
-										className='block absolute cursor-pointer right-0'
-										onClick={() => removeImage(image)}
-									>
-										<svg
-											xmlns='http://www.w3.org/2000/svg'
-											fill='none'
-											viewBox='0 0 24 24'
-											strokeWidth={1.5}
-											stroke='currentColor'
-											className='w-6 h-6'
-										>
-											<path
-												strokeLinecap='round'
-												strokeLinejoin='round'
-												d='M6 18L18 6M6 6l12 12'
-											/>
-										</svg>
-									</button>
+					<option value=''>Uncategorized</option>
+					{categoriesList.length > 0
+						? categoriesList.map(categoryItem => (
+							<option key={categoryItem._id} value={categoryItem._id}>
+								{categoryItem.name}
+							</option>
+						))
+						: null}
+				</select>
+				<div className='pt-2'>
+					{propertiesToFill.length > 0
+						? propertiesToFill.map(p => (
+							<div
+								key={p.name}
+								className='flex flex-row items-baseline gap-1'
+							>
+								<div className='flex'>
+									<label>
+										{p.name[0].toUpperCase() + p.name.substring(1)}
+									</label>
 								</div>
-								<SortableItem>
-									<img
-										className='w-24 min h-24 min-w-24 object-cover min-h-24 border rounded-sm bg-white cursor-grab active:cursor-grabbing'
-										src={image}
-										alt={'product image'}
-										width={60}
-										height={60}
-									></img>
-								</SortableItem>
+								<div className='flex'>
+									<select
+										value={productProperties[p.name]}
+										onChange={ev =>
+											setProductProps(p.name, ev.target.value)
+										}
+									>
+										<option value=''>Property value</option>
+										{p.values.map(value => (
+											<option key={value} value={value}>
+												{value}
+											</option>
+										))}
+									</select>
+								</div>
 							</div>
 						))
 						: null}
-				</SortableList>
-			</div>
-			<label>Description</label>
-			<textarea
-				placeholder='description'
-				className='max-h-52'
-				value={description}
-				onChange={ev => setDescription(ev.target.value)}
-			></textarea>
-			<label>Price (in €)</label>
-			<input
-				type='text'
-				placeholder='price'
-				value={price}
-				onChange={ev => setPrice(ev.target.value)}
-			></input>
-			<button type='submit' className='btn-primary'>
-        Save
-			</button>
-		</form>
+				</div>
+
+				<label>Images</label>
+				<div className='flex gap-1 flex-row flex-wrap mb-3'>
+					<label className='text-primary w-24 h-24 border flex items-center justify-center text-sm gap-1 rounded-sm bg-white border-primary shadow-sm cursor-pointer'>
+            Upload{' '}
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							strokeWidth='1.5'
+							stroke='currentColor'
+							className='w-4 h-4'
+						>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5'
+							/>
+						</svg>
+						<input type='file' className='hidden'></input>
+					</label>
+					{isUploading ? (
+						<label className='w-24 h-24 border flex items-center justify-center text-sm gap-1 rounded-sm bg-gray-200'>
+							<div className='activityIndicator'></div>
+						</label>
+					) : null}
+
+					<SortableList
+						onSortEnd={onSortEnd}
+						className='list flex gap-1 flex-row flex-wrap'
+						draggedItemClassName='dragged'
+					>
+						{images?.length > 0
+							? images.map(image => (
+								<div key={image}>
+									<div className='p-0 m-0 block relative'>
+										<button
+											className='block absolute cursor-pointer right-0'
+											onClick={() => removeImage(image)}
+										>
+											<svg
+												xmlns='http://www.w3.org/2000/svg'
+												fill='none'
+												viewBox='0 0 24 24'
+												strokeWidth={1.5}
+												stroke='currentColor'
+												className='w-6 h-6'
+											>
+												<path
+													strokeLinecap='round'
+													strokeLinejoin='round'
+													d='M6 18L18 6M6 6l12 12'
+												/>
+											</svg>
+										</button>
+									</div>
+									<SortableItem>
+										<img
+											className='w-24 min h-24 min-w-24 object-cover min-h-24 border rounded-sm bg-white cursor-grab active:cursor-grabbing'
+											src={image}
+											alt={'product image'}
+											width={60}
+											height={60}
+										></img>
+									</SortableItem>
+								</div>
+							))
+							: null}
+					</SortableList>
+				</div>
+				<label>Description</label>
+				<textarea
+					placeholder='description'
+					className='max-h-52'
+					value={description}
+					onChange={ev => setDescription(ev.target.value)}
+				></textarea>
+				<label>Price (in €)</label>
+				<input
+					type='text'
+					placeholder='price'
+					value={price}
+					onChange={ev => setPrice(ev.target.value)}
+				></input>
+				<button type='submit' className='btn-primary'>
+          Save
+				</button>
+			</form>
+			{showModal ? (
+				<div className='flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-blur-sm'>
+					<div className='relative w-auto my-6 mx-auto max-w-3xl border-0 rounded-sm shadow-lg flex flex-col bg-white outline-none focus:outline-none'>
+						<p className='text-center text-lg font-semibold p-6'>
+							{fetchSuccess
+								? 'Product saved successfully.'
+								: 'An error has ocurred. Could not save changes.'}
+						</p>
+
+						<div className='flex items-center justify-center p-6 gap-2'>
+							<button
+								className='btn-primary'
+								type='button'
+								onClick={() => setShowModal(false)}
+							>
+                Continue editing
+							</button>
+							<button
+								className='btn-primary'
+								type='button'
+								onClick={() => setGoBack(true)}
+							>
+								{fetchSuccess
+									? 'Back to products.'
+									: 'Try again later.'}
+
+							</button>
+						</div>
+					</div>
+				</div>
+			) : null}
+		</>
 	);
 }
